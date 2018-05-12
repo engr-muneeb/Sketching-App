22a9b0914e6f2615dbbb3f9a6d7ff0e2455bb35d
Engr Muneeb <38957175+engr-muneeb@users.noreply.github.com>
2018-05-13 02:52:37 +0500

    Update App.js

--- Sketching-App/client/src/App.js
+++ Sketching-App/client/src/App.js
@@ -22,7 +22,7 @@ class App extends Component {
     return (
       <div className="App">
         <div className="App-header">
-          <h2>Our awesome sketchin app</h2>
+          <h2>Our awesome sketching app</h2>
         </div>
         This is the value of the timer timestamp: {this.state.timestamp}      
       </div>
