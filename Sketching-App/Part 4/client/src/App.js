f4be615db61e43dcd0f223d204b8b117d1c64397
Engr Muneeb <38957175+engr-muneeb@users.noreply.github.com>
2018-05-17 19:04:31 +0500

    Add files via upload
    Part 4 of sketching app

---
+++ Sketching-App/Part 4/client/src/App.js
@@ -0,0 +1,49 @@
+import React, { Component } from 'react';
+import './App.css';
+import SketchingForm from './SketchingForm';
+import SketchingList from './SketchingList';
+import Sketching from './Sketching';
+
+class App extends Component {
+  state = {
+  };
+
+  selectSketching = (sketching) => {
+    this.setState({
+      selectedSketching: sketching,
+    });
+  }
+
+  render() {
+    let ctrl = (
+      <div>
+        <SketchingForm />
+
+        <SketchingList
+          selectSketching={this.selectSketching}
+        />
+      </div>
+    );
+
+    if (this.state.selectedSketching) {
+      ctrl = (
+        <Sketching
+          sketching={this.state.selectedSketching}
+          key={this.state.selectedSketching.id}
+        />
+      );
+    }
+
+    return (
+      <div className="App">
+        <div className="App-header">
+          <h2>Our awesome sketching app</h2>
+        </div>
+
+        { ctrl }
+      </div>
+    );
+  }
+}
+
+export default App;
