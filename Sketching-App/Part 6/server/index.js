57c39439f775bb6188dcd4e0724d6ddc67252480
Engr Muneeb <38957175+engr-muneeb@users.noreply.github.com>
2018-05-20 19:06:36 +0500

    Add files via upload
    This contain the part 6 of the tutorial

---
+++ Sketching-App/Part 6/server/index.js
@@ -0,0 +1,81 @@
+const r = require('rethinkdb');
+const io = require('socket.io')();
+
+function createSketching({ connection, name }) {
+  return r.table('sketchings')
+  .insert({
+    name,
+    timestamp: new Date(),
+  })
+  .run(connection)
+  .then(() => console.log('created a new sketching with name ', name));
+}
+
+function subscribeToSketchings({ client, connection }) {
+  r.table('sketchings')
+  .changes({ include_initial: true })
+  .run(connection)
+  .then((cursor) => {
+    cursor.each((err, sketchingRow) => client.emit('sketching', sketchingRow.new_val));
+  });
+}
+
+function handleLinePublish({ connection, line }) {
+  console.log('saving line to the db')
+  r.table('lines')
+  .insert(Object.assign(line, { timestamp: new Date() }))
+  .run(connection);
+}
+
+function subscribeToSketchingLines({ client, connection, sketchingId, from }) {
+  let query = r.row('sketchingId').eq(sketchingId);
+
+  if (from) {
+    query = query.and(r.row('timestamp').ge(new Date(from)))
+  }
+
+  return r.table('lines')
+  .filter(query)
+  .changes({ include_initial: true, include_types: true })
+  .run(connection)
+  .then((cursor) => {
+    cursor.each((err, lineRow) => client.emit(`sketchingLine:${sketchingId}`, lineRow.new_val));
+  });
+}
+
+r.connect({
+  host: 'localhost',
+  port: 28015,
+  db: 'awesome_whiteboard'
+}).then((connection) => {
+  io.on('connection', (client) => {
+    client.on('createSketching', ({ name }) => {
+      createSketching({ connection, name });
+    });
+
+    client.on('subscribeToSketchings', () => subscribeToSketchings({
+      client,
+      connection,
+    }));
+
+    client.on('publishLine', (line) => handleLinePublish({
+      line,
+      connection,
+    }));
+
+    client.on('subscribeToSketchingLines', ({ sketchingId, from }) => {
+      subscribeToSketchingLines({
+        client,
+        connection,
+        sketchingId,
+        from,
+      });
+    });
+  });
+});
+
+
+const port = parseInt(process.argv[2], 10) || 8000;
+io.listen(port);
+console.log('listening on port ', port);
+
